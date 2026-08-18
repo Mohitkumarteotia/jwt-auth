cd279628e813a9a756d894c30b64a4a41c69033a
MohitKumarteotia <teotiamohit011@gmail.com>
2026-08-18 15:35:13 +0530

    Update

---
+++ src/main/java/com/service/jwt_auth/entity/User.java
@@ -0,0 +1,23 @@
+package com.service.jwt_auth.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Entity
+@Table(name = "users")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(name = "name", nullable = false)
+    private String name;
+    @Column(name = "password", nullable = false)
+    private String password;
+}
---
+++ src/main/java/com/service/jwt_auth/repository/UserRepository.java
@@ -0,0 +1,9 @@
+package com.service.jwt_auth.repository;
+
+import com.service.jwt_auth.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+}

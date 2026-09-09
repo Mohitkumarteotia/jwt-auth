fcf52ec6c6c2b33fcf7f02921742db8fcac5f3d9
MohitKumarteotia <teotiamohit011@gmail.com>
2026-09-09 17:24:05 +0530

    Update

--- src/main/java/com/service/jwt_auth/exception/GlobalExceptionHandler.java
+++ src/main/java/com/service/jwt_auth/exception/GlobalExceptionHandler.java
@@ -6,8 +6,6 @@ import com.service.jwt_auth.pojos.response.ErrorResponse;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.security.access.AccessDeniedException;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
-
-
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.MethodArgumentNotValidException;
